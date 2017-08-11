246535223c39ad947f615ac12d22a66d3aaf513f
Tanveer Singh <tanveer7537@gmail.com>
2017-08-11 18:46:35 +0530

    Made a seperate vie for the semi transparent text field

---
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,29 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Tanveer Singh on 11/08/17.
+//  Copyright © 2017 Tanveer Singh. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.2498394692)
+        layer.cornerRadius = 7.0
+        textAlignment = .center
+        
+        if placeholder == nil{
+            placeholder = " "
+        }//safety check for force unwrapping
+        
+        let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        
+        attributedPlaceholder = place
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+
+}
