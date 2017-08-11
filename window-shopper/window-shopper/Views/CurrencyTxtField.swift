a0937333e3fb0c30ba857756645173235e1fab20
Tanveer Singh <tanveer7537@gmail.com>
2017-08-11 22:09:28 +0530

    @IBDesignable and prepareForInterfaceBuilder() added in custom cocoa touch class ie CurrencyTxtField.swift

--- window-shopper/window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+@IBDesignable
 
 class CurrencyTxtField: UITextField {
-
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView(){
         backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.2498394692)
         layer.cornerRadius = 7.0
         textAlignment = .center
@@ -25,5 +33,7 @@ class CurrencyTxtField: UITextField {
         attributedPlaceholder = place
         textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     }
+    
+    
 
 }
